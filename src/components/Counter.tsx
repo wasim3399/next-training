259a2468a147f174f0f3bd9b8b84719acb060df8
Wasim Rashid <wasim.rashid@developers.studio>
2023-07-18 23:10:12 +0500

    integrate redux

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import type { RootState } from "@/store/store";
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "@/store/slice/counterSlice";
 
 export default function Counter() {
+  const count = useSelector((state: RootState) => state.counterSlice.value);
+  const dispatch = useDispatch();
   return (
-    <div>Counter</div>
-  )
+    <div className="flex flex-row gap-8 justify-center mt-10">
+      <Button
+        variant={"destructive"}
+        onClick={() => dispatch(counterActions.decrement())}
+      >
+        Decrement
+      </Button>
+
+      <span className="text-lg leading-10 font-semibold">{count}</span>
+      <Button
+        variant={"secondary"}
+        onClick={() => {
+          dispatch(counterActions.increment());
+        }}
+      >
+        Increment
+      </Button>
+    </div>
+  );
 }
